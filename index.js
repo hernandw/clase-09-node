4f34788f3f53c3a434bdb0513fd0df84bbfd9c5c
Williams Hernandez <hernandw@gmail.com>
2024-03-26 12:58:30 -0300

    :tada: configuracion de nodemailer y levantamiento del servidor

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-const nodemailer = require('nodemailer');
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send('Hola, soy una nueva ruta');
+})
+
+app.get('/enviar', (req, res) => {
+    res.send('Hola, soy una nueva ruta');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
+})
 
 
-// Configuración de Gmail
-let transporter = nodemailer.createTransport({
-  service: "gmail", // Puedes usar cualquier otro servicio
-  auth: {
-    user: process.env.GMAIL_USER, // Tu dirección de correo
-    pass: process.env.GMAIL_PASSWORD, // Tu contraseña de correo
-  },
-});
 
 
 
-// Opciones del correo electrónico
-  let mailOptions = {
-    from: process.env.GMAIL_USER, // De quién
-    to:   process.env.GMAIL_USER,   //Para quién
-    subject: "mensaje de prueba", // Asunto del correo
-    html: "<h1>Mensaje de prueba desde nodemailer version 2</h1>", // Contenido del correo en HTML
-  };
 
 
-  // Enviar el correo electrónico
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-    console.log("Mensaje enviado: %s", info.messageId);
-  });
+
+
+ 
 
---
+++ mail/config.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+// Configuración de Gmail
+let transporter = nodemailer.createTransport({
+  service: "gmail", // Puedes usar cualquier otro servicio
+  auth: {
+    user: process.env.GMAIL_USER, // Tu dirección de correo
+    pass: process.env.GMAIL_PASSWORD, // Tu contraseña de aplicaciones
+  },
+});
+
+const enviarCorreo = (to, subject, html) => {
+  // Opciones del correo electrónico
+  let mailOptions = {
+    from: process.env.GMAIL_USER, // De quién
+    to, //Para quién
+    subject, // Asunto del correo
+    html, // Contenido del correo en HTML
+  };
+
+  // Enviar el correo electrónico
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Mensaje enviado: %s", info.messageId);
+  });
+};
+
+module.exports= enviarCorreo
